00c562a77f30dfc8caa21281fbbba97775ad5b98
omsingh-coder <omsinghvmc@gmail.com>
2025-12-11 12:43:57 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+// Simple Express + Socket.IO server that serves the static index.html and handles sockets via socket-handler.js
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const createSocketHandler = require('./socket-handler');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// serve static files from same folder
+app.use(express.static(path.join(__dirname, '/')));
+
+createSocketHandler(io);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(Server running at http://localhost:${PORT});
+  console.log(Make sure to set MASTER_KEY env var (32 bytes) for secret encryption.);
+});
